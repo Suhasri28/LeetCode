bbe2eb15abb45874d8111d648355d3758c9cb83a
Suhasri28 <110117501+Suhasri28@users.noreply.github.com>
2025-10-24 19:29:43 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (97.55%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
     void sortColors(vector<int>& arr) {
-        int i = 0, j = 0;
+        int i = 0, j = arr.size() - 1;
         int mid = 0;
-        // while(mid <= j) {
-        //     if(arr[mid] == 0) {
-        //         swap(arr[mid], arr[i]);
+        
+        while(mid <= j) {
+            if(arr[mid] == 0) {
+                swap(arr[i], arr[mid]);
+                i++;
+                mid++;
+            }
+            else if(arr[mid] == 2) {
+                swap(arr[mid], arr[j]);
+                j--;
+            }
+            else{
+                mid++;
+            }
+        }
+
+        // while(j < arr.size()) {
+        //     if(arr[j] == 0){
+        //         swap(arr[i], arr[j]);
         //         i++;
-        //         mid++;
         //     }
-        //     else if(arr[mid] == 2) {
-        //         swap(arr[mid], arr[j]);
-        //         j--;
-        //     }
-        //     else{
-        //         mid++;
-        //     }
+        //     j++;
         // }
+        // j = i;
 
-        while(j < arr.size()) {
-            if(arr[j] == 0){
-                swap(arr[i], arr[j]);
-                i++;
-            }
-            j++;
-        }
-        j = i;
-
-        while(j < arr.size()){
-            if(arr[j] == 1) {
-                swap(arr[i], arr[j]);
-                i++;
-            }
-            j++;
-        }
+        // while(j < arr.size()){
+        //     if(arr[j] == 1) {
+        //         swap(arr[i], arr[j]);
+        //         i++;
+        //     }
+        //     j++;
+        // }
     }
 };
